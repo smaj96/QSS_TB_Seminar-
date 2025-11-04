c14183793df2edb5182cbbfafa3518a6fa2b90f8
wet56myv <wet56myv@ctx7rhrk-tsa062>
2025-11-04 16:46:16 +0100

    cleaning the code

--- Functions/ftp_controller1.m
+++ Functions/ftp_controller1.m
@@ -5,31 +5,25 @@ function ctl_out = ftp_controller1(input)
 %pulled
 %answer by smaj96 : wet56myv make branch to modify the push!!! 2nd.
 %wet : ok sir.
+% by Wet, 4,11,2025. i will made some change ( cleaning the code remove not used lines   )
+
+%Input
 w_MGB = input(1);            % Angular velocity at torque combiner/gearbox
 dw_MGB = input(2);           %  angular acceleration (rate of change of W_MGB)
 T_MGB = input(3);            % Total torque demand at the gearbox input 
-Q_BT = input(4);            % Battery charge[C]
-%i = input(4);            % 4
-%v = input(5);            % 5
-
-global w_EM_max;             % maximum motor angular velocity [rads/s](global) 
-global T_EM_max;             %  maximum motor torque[Nm] (global)
-theta_EM = 0.1;              %  motor rotor inertia[kg.m2]
-
-T_MGB_th = 35;              % define torque threshold for NEDC 
-
-eps = 0.01;              % small buffer value for numerical robustness
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Q_BT = input(4);             % Battery charge[C]
 
 % --- Parameters ---
+global w_EM_max;             %  maximum motor angular velocity [rads/s](global) 
+global T_EM_max;             %  maximum motor torque[Nm] (global)
+theta_EM = 0.1;              %  motor rotor inertia[kg.m2]
+eps = 0.01;                  %  small buffer value for numerical robustness
+
 Q_min = 3600;        % Minimum allowable battery charge [C] 
 Q_max = 32400;  %15     % Maximum allowable battery charge [C]
 Q_low  = Q_min + 1;  % Hysteresis buffer to prevent rapid switching 
 T_thresh = 32; %16.5   % Threshold for electric-only mode
 u_LPS_max = 0.001;  % Max torque split ratio in LPS
-dw_thresh = 0.1;  % Optional: acceleration threshold
 
 % --- Default outputs ---
 state_CE = 0;
@@ -55,7 +49,6 @@ if T_MGB > 0
         state_CE = 1;
         u = 0;
     end
-
 % --- Regeneration ---
 elseif T_MGB < 0
     if Q_BT < Q_max
